8926fb142192d1dac570723e2962e25838699efd
sttng <46765778+sttng@users.noreply.github.com>
2025-11-07 03:05:09 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -22,17 +22,18 @@ container_id=$(docker create sd2snes-fpga)
 docker cp ${container_id}:/work/sd2snes/ ./out/
 
 echo Extracting im3 files
-docker cp ${container_id}:/work/sd2snes/src/obj-mk3/firmware.im3 ./out/
-docker cp ${container_id}:/work/sd2snes/src/obj-mk3-stm32/firmware.stm ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_base/fpga_base.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_mini/fpga_mini.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_dsp/fpga_dsp.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_cx4/fpga_cx4.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_gsu/fpga_gsu.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_obc1/fpga_obc1.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_sa1/fpga_sa1.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_sdd1/fpga_sdd1.bi3 ./out/
-docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_sgb/fpga_sgb.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/src/obj-mk3/firmware.im3 ./out/
+#docker cp ${container_id}:/work/sd2snes/src/obj-mk3-stm32/firmware.stm ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_base/fpga_base.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_mini/fpga_mini.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_dsp/fpga_dsp.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_cx4/fpga_cx4.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_gsu/fpga_gsu.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_obc1/fpga_obc1.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_sa1/fpga_sa1.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_sdd1/fpga_sdd1.bi3 ./out/
+#docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_sgb/fpga_sgb.bi3 ./out/
+docker cp ${container_id}:/work/sd2snes/verilog/sd2snes_spc7110/fpga_spc7110.bi3 ./out/
 echo Removing temporary container
 docker rm ${container_id}
 echo Successfully built firmware and FPGA images for MK3
